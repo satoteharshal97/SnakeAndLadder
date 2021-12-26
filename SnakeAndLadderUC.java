ef04e0e10f968942328dbab53cfc7fbe4b9e2490
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2021-12-26 18:05:56 +0530

    Adde - Switch Case

--- SnakeAndLadderUC.java
+++ SnakeAndLadderUC.java
@@ -1,9 +1,26 @@
 public class SnakeAndLadderUC {
+	public static final int NO_PlAY = 1;
+    public static final int LADDER = 2;
+    public static final int SNAKE = 3;
 	
     public static void main(String[] args) {
         int startPosition = 0;
-        System.out.println("Player is at initial position: " + startPosition);
-		
-		
+        int playerPosition = 0;
+        int rollDice = (int) Math.floor(Math.random() * 6 + 1);   
+        int option = (int) Math.floor(Math.random() * 3 + 1);
+ 
+        switch (option) {
+            case NO_PlAY:
+                System.out.println("Player is at same position");
+                break;
+            case LADDER:
+                playerPosition = playerPosition + rollDice;
+                System.out.println("Player's position is increased by: " + rollDice);    
+                break;
+            case SNAKE:
+                System.out.println("Player's position is decreased by: " + rollDice);
+                break;
+        }     
+        
     }
 }
