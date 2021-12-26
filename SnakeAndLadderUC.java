f63b00f542aad3ba8e824e3c61dd9e239d081dc4
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2021-12-26 17:51:23 +0530

    Added - Intialized game position

---
+++ SnakeAndLadderUC.java
@@ -0,0 +1,9 @@
+public class SnakeAndLadderUC {
+	
+    public static void main(String[] args) {
+        int startPosition = 0;
+        System.out.println("Player is at initial position: " + startPosition);
+		
+		
+    }
+}
