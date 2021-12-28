41a87bad748d5d0b01e34c58aac58083582c739b
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2021-12-29 01:32:37 +0530

    Added - Added dice in game

--- SnakeAndLadderUC.java
+++ SnakeAndLadderUC.java
@@ -2,6 +2,8 @@ public class SnakeAndLadderUC {
 	
     public static void main(String[] args) {
         int startPosition = 0;
+	int rollDice = (int) Math.floor(Math.random() * 6 + 1);
         System.out.println("Player is at initial position: " + startPosition);
+	System.out.println("After rolling dice we get : " + rollDice);	
     }
 }
