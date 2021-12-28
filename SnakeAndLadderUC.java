b41f13f43e0bff9639777794a7603e7bf254cc5a
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2021-12-29 01:29:58 +0530

    Added - Intialize start position

--- SnakeAndLadderUC.java
+++ SnakeAndLadderUC.java
@@ -1,6 +1,7 @@
 public class SnakeAndLadderUC {
 	
     public static void main(String[] args) {
-	System.out.println("Welcome to snake and ladder game");	
+        int startPosition = 0;
+        System.out.println("Player is at initial position: " + startPosition);
     }
 }
