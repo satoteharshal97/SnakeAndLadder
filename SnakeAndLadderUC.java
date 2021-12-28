391250633400a4f57a0a3296e6e91986ebea2984
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2021-12-29 01:47:09 +0530

    Added - Now Player has reached excat winning position

--- SnakeAndLadderUC.java
+++ SnakeAndLadderUC.java
@@ -1,18 +1,14 @@
 public class SnakeAndLadderUC {
-	
-    public static final int NO_PlAY = 1;
+    public static final int NO_PLAY = 1;
     public static final int LADDER = 2;
     public static final int SNAKE = 3;
     public static final int WINNING_POSITION = 100;
 
     public static void main(String[] args) {
-	    
         int startPosition = 0;
         int playerPosition = 0;
         int diceCount = 0;
-	    
         while(playerPosition < WINNING_POSITION) {
-		
             int rollDice = (int) Math.floor(Math.random() * 6 + 1);
             int option = (int) Math.floor(Math.random() * 3 + 1);
 
@@ -22,7 +18,7 @@ public class SnakeAndLadderUC {
                 case LADDER:
                     playerPosition = playerPosition + rollDice;
                     if(playerPosition > 100){
-                        playerPosition = playerPosition -rollDice;
+                        playerPosition = playerPosition - rollDice;
                     }
                     break;
                 case SNAKE:
@@ -33,6 +29,6 @@ public class SnakeAndLadderUC {
                     break;
             }
         }
-	System.out.println("  Winning Position:" + playerPosition);
+	System.out.println("Player has reached has reached exact Winning position!:" + playerPosition);
     }
 }
