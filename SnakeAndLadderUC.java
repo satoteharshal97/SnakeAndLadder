0f22af5cd5dc6340758f48792544eeb25cbadf9d
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2021-12-29 01:42:44 +0530

    Added - Repeating loop till player reaches winning position

--- SnakeAndLadderUC.java
+++ SnakeAndLadderUC.java
@@ -1,25 +1,38 @@
-
 public class SnakeAndLadderUC {
-    public static final int NO_PLAY = 1;
+	
+    public static final int NO_PlAY = 1;
     public static final int LADDER = 2;
     public static final int SNAKE = 3;
-	
+    public static final int WINNING_POSITION = 100;
+
     public static void main(String[] args) {
+	    
         int startPosition = 0;
         int playerPosition = 0;
-        int rollDice = (int) Math.floor(Math.random() * 6 + 1);   
-        int option = (int) Math.floor(Math.random() * 3 + 1);
- 
-        switch (option) {
-            case NO_PLAY:
-                System.out.println("Player is at same position");
-                break;
-            case LADDER:
-                System.out.println("Player's position is increased by: " + rollDice);    
-                break;
-            case SNAKE:
-                System.out.println("Player's position is decreased by: " + rollDice);
-                break;
-        }            
+        int diceCount = 0;
+	    
+        while(playerPosition < WINNING_POSITION) {
+		
+            int rollDice = (int) Math.floor(Math.random() * 6 + 1);
+            int option = (int) Math.floor(Math.random() * 3 + 1);
+
+            switch (option) {
+                case NO_PLAY:
+                    break;
+                case LADDER:
+                    playerPosition = playerPosition + rollDice;
+                    if(playerPosition > 100){
+                        playerPosition = playerPosition -rollDice;
+                    }
+                    break;
+                case SNAKE:
+                    playerPosition = playerPosition - rollDice;
+                    if(playerPosition < 0){
+                        playerPosition = startPosition;
+                    }
+                    break;
+            }
+        }
+	System.out.println("  Winning Position:" + playerPosition);
     }
 }
