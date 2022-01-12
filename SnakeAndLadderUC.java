85d8285ec4e5b9ded6992c349ccbabb3ae091f6a
Harshal Satote <37376689+satoteharshal97@users.noreply.github.com>
2022-01-12 23:26:30 +0530

    Refactor - Made code more modular

--- SnakeAndLadderUC.java
+++ SnakeAndLadderUC.java
@@ -6,39 +6,22 @@ public class SnakeAndLadderUC {
     public static final int SNAKE = 2;
     public static final int WINNING_POSITION = 100;
     public static final int START_POSITION = 0;
+    public static int chance = 0;
+    public static int playerOnePosition = 0;
+    public static int playerTwoPosition = 0;
+    public static int playerPosition = 0;
 
     public static void main(String[] args) {
-        int playerOnePosition = 0;
-        int playerTwoPosition = 0;
-        int playerPosition = 0;
-        int chance = 0;
+        switchPlayer();
+    }
+
+
+    public static void switchPlayer(){
 
         while (playerOnePosition < WINNING_POSITION && playerTwoPosition < WINNING_POSITION) {
             if (chance % 2 == 0) {
                 playerPosition = playerOnePosition;
-                int rollDice = (int) Math.floor(Math.random() * 6 + 1);
-                int option = (int) Math.floor(Math.random() * 3);
-
-                switch (option) {
-                    case NO_PLAY:
-                        playerPosition = playerPosition;
-                        System.out.println("Player one is at same position");
-                        break;
-                    case LADDER:
-                        chance++;
-                        playerPosition = playerPosition + rollDice;
-                        System.out.println("Player one position is increased by : " + rollDice);
-                        if (playerPosition > 100) {
-                            playerPosition = playerPosition - rollDice;
-                        }
-                        break;
-                    case SNAKE:
-                        playerPosition = playerPosition - rollDice;
-                        System.out.println("Player one position is decreased by : " + rollDice);
-                        if (playerPosition < 0) {
-                            playerPosition = START_POSITION;
-                        }
-                }
+                playGame();
                 playerOnePosition = playerPosition;
                 if (playerOnePosition == WINNING_POSITION) {
                     System.out.println("Yey!Player One Won");
@@ -46,35 +29,38 @@ public class SnakeAndLadderUC {
                 }
             } else {
                 playerPosition = playerTwoPosition;
-                int rollDice = (int) Math.floor(Math.random() * 6 + 1);
-                int option = (int) Math.floor(Math.random() * 3);
-
-                switch (option) {
-                    case NO_PLAY:
-                        playerPosition = playerPosition;
-                        System.out.println("Player two is at same position");
-                        break;
-                    case LADDER:
-                        chance++;
-                        playerPosition = playerPosition + rollDice;
-                        System.out.println("Player two position is increased by : " + rollDice);
-                        if (playerPosition > 100) {
-                            playerPosition = playerPosition - rollDice;
-                        }
-                        break;
-                    case SNAKE:
-                        playerPosition = playerPosition - rollDice;
-                        System.out.println("Player two position is decreased by : " + rollDice);
-                        if (playerPosition < 0) {
-                            playerPosition = START_POSITION;
-                        }
-                }
+                playGame();
                 playerTwoPosition = playerPosition;
                 if (playerTwoPosition == WINNING_POSITION) {
                     System.out.println("Yey! Player Two Won");
                     break;
                 }
             }
+        }
+    }
+    public static void playGame(){
+        int rollDice = (int) Math.floor(Math.random() * 6 + 1);
+        int option = (int) Math.floor(Math.random() * 3);
+
+        switch (option) {
+            case NO_PLAY:
+                playerPosition = playerPosition;
+                System.out.println("Player  is at same position");
+                break;
+            case LADDER:
+                chance++;
+                playerPosition = playerPosition + rollDice;
+                System.out.println("Player position is increased by : " + rollDice);
+                if (playerPosition > 100) {
+                    playerPosition = playerPosition - rollDice;
+                }
+                break;
+            case SNAKE:
+                playerPosition = playerPosition - rollDice;
+                System.out.println("Player  position is decreased by : " + rollDice);
+                if (playerPosition < 0) {
+                    playerPosition = START_POSITION;
+                }
         }
     }
 }
